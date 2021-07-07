c4b0f0232406dc7d24cedbd09f6289c67a74f5fc
MegSteg <86500966+MegSteg@users.noreply.github.com>
2021-07-07 13:48:09 +1000

    c

---
+++ user_input_playground.py
@@ -0,0 +1,11 @@
+answer = input("How are you? ")
+message= f"i'm {answer} as well, thanks for asking"
+print(message)
+
+name = input ("what is your name?")
+hobby = input ("What is your hobby")
+print(f"This is {name}, like {hobby}")
+
+age = input (f"Hi {name}, how old are you?")
+years_until_100 = 100 - int(age)
+print(f"Wow, {name}, You'll be 100 in {years_until_100}")
---
+++ variables_playground.py
@@ -0,0 +1,68 @@
+day = "Saturday"
+message = f"Today is {day}"
+print (message)
+print(type(day))
+month = "September"
+message2 = f"And my favourite month is {month}"
+print(message2)
+print(type(day))
+
+apples = 5
+banana = 8
+fruits = apples + banana
+print (fruits)
+print (type(fruits))
+
+height= 1.70
+weight= 999.9
+calculation = weight - height
+print (calculation)
+print(type(calculation))
+
+run1_dist= 1400
+run2_dist= 1800
+
+total_distance = run1_dist + run2_dist
+print(total_distance)
+
+run3_dist = 1.7
+run4_dist = 1.35
+
+total_distance = run3_dist / run4_dist
+example_distance = run1_dist/ run2_dist
+print(total_distance)
+print(example_distance)
+
+example_distance = run1_dist /run2_dist
+
+#
+goal_dist= 1000
+run1_left= goal_dist-run1_dist
+print (run1_left)
+
+
+var1 = "banana"
+var2 = 5
+var3 = "3"
+var4= 10
+
+print(var1 + str(var2))
+
+print(var2 +int(var3))
+
+print(var1 * var2)
+
+print(float(var2))
+
+calculation = var4 / var2
+print(calculation)
+
+run5_dist = "5000"
+#print(run5_dist + 3) #error because string and integer
+#print(run5_dist + "3") #error
+#print(run5_dist * 3) #5000 in a string
+#print(run5_dist* 3.0) #error because string multiplied by float
+
+print(int(run5_dist)+ 3)
+print(str(3))
+#should be
